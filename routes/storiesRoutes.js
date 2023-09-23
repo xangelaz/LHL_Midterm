232d02e00feed088f3418b37ad78dd62d782491f
markmdoza <marklmdoza@gmail.com>
2023-09-23 10:53:08 -0400

    Tried to append contributions

--- routes/storiesRoutes.js
+++ routes/storiesRoutes.js
@@ -5,15 +5,15 @@
 const express = require('express');
 const { getAllStories, getStoriesByUserId, getStory, addStory, storyComplete } = require('../db/queries/stories');
 const { getAllContributions } = require('../db/queries/contributions');
-const router  = express.Router();
+const router = express.Router();
 
 
 // Get all stories.
 // to be used when loading the home page.
 router.get('/', (req, res) => {
   getAllStories().then((allStories) => {
-    res.render('index', { allStories })
-  })
+    res.render('index', { allStories });
+  });
 });
 
 // Gets story and all contributions for that story.
@@ -22,19 +22,19 @@ router.get('/:id', (req, res) => {
   const storyId = req.params.id;
   getStory(storyId).then((story) => {
     getAllContributions(storyId).then((contributions) => {
-      console.log('contributions', contributions)
-      res.render('contributions', { story, contributions })
-    })
-  })
+      console.log('contributions', contributions);
+      res.render('contributions', { story, contributions });
+    });
+  });
 });
 
 // Adds a new story to database and redirects to home page.
 router.post('/', (req, res) => {
   const contents = req.body.story;
   const title = req.body.title;
-  addStory({contents, title})
+  addStory({ contents, title })
     .then((story) => {
-      res.redirect('/')
+      res.redirect('/');
     })
     .catch((e) => {
       console.error(e);
@@ -44,7 +44,7 @@ router.post('/', (req, res) => {
 
 router.post('/:id/status', (req, res) => {
   const id = req.params.id;
-  console.log('post for id/status', req.params.id)
+  console.log('post for id/status', req.params.id);
   storyComplete(id)
     .catch((e) => {
       console.error(e);
@@ -58,7 +58,7 @@ router.post('/:id/status', (req, res) => {
 router.get('/my_stories/:id', (req, res) => {
   const userId = req.params.id;
   getStoriesByUserId(userId).then((userStories) => {
-    res.render('my_stories', { userStories })
-  })
+    res.render('my_stories', { userStories });
+  });
 });
 module.exports = router;
